15e332ff992f9e8183e29fecb96340dfe5013546
soumen Nath <saumennath29@gmail.com>
2023-08-20 19:50:27 +0530

    add sample home page

---
+++ src/app/features/shared-modules/shared-modules.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SharedModulesComponent } from './shared-modules.component';
+
+describe('SharedModulesComponent', () => {
+  let component: SharedModulesComponent;
+  let fixture: ComponentFixture<SharedModulesComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ SharedModulesComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(SharedModulesComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
