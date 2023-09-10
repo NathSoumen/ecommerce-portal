c7ed873bf8f53789f992687a33617a87c1311cd5
soumen Nath <saumennath29@gmail.com>
2023-09-10 14:27:08 +0530

    add login page

--- src/app/features/shared-modules/product-card/product-card.component.ts
+++ src/app/features/shared-modules/product-card/product-card.component.ts
@@ -1,41 +1,45 @@
-import { Component, OnChanges, SimpleChanges ,Input} from '@angular/core';
+import { Component, OnChanges, SimpleChanges, Input } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-product-card',
   templateUrl: './product-card.component.html',
-  styleUrls: ['./product-card.component.scss']
+  styleUrls: ['./product-card.component.scss'],
 })
 export class ProductCardComponent implements OnChanges {
-  @Input()product :any
-  productName:string = ''
-  originalPrice:number = 0;
-  discountedPrice:number = 0
-  currencySymbal :string = '$'
-  imageUrl :string = ''
+  @Input() product: any;
+  productName: string = '';
+  originalPrice: number = 0;
+  discountedPrice: number = 0;
+  currencySymbal: string = '$';
+  imageUrl: string = '';
+  constructor(private router:Router){}
   ngOnChanges(changes: SimpleChanges): void {
-    if(changes["product"]) {
-      this.productName = this.product.name
-      this.originalPrice = this.product.originalPrice
-      this.discountedPrice = this.product.discountedPrice
-      this.imageUrl = this.product.imageUrl
+    if (changes['product']) {
+      this.productName = this.product.name;
+      this.originalPrice = this.product.originalPrice;
+      this.discountedPrice = this.product.discountedPrice;
+      this.imageUrl = this.product.imageUrl;
     }
-  } 
-  addTocart(){
-//     const foundToken = localStorage.getItem("token")
-//     if(foundToken) {
-// const token = JSON.parse(foundToken);
-// console.log("token",token);
-
-//     }
-       const foundProduct= localStorage.getItem("addedToCart")
-       if(foundProduct) {
-        const  addedToCartsProducts:Array<any> =  JSON.parse(foundProduct);
-        addedToCartsProducts.push(this.product)
-        localStorage.setItem("addedToCart",JSON.stringify(addedToCartsProducts))
-
-       }else{
-        localStorage.setItem("addedToCart",JSON.stringify([this.product]))
-       }
   }
-
+  addTocart() {
+    const foundToken = localStorage.getItem('token');
+    if (foundToken) {
+      const token = JSON.parse(foundToken);
+      console.log('token', token);
+      const foundProduct = localStorage.getItem('addedToCart');
+      if (foundProduct) {
+        const addedToCartsProducts: Array<any> = JSON.parse(foundProduct);
+        addedToCartsProducts.push(this.product);
+        localStorage.setItem(
+          'addedToCart',
+          JSON.stringify(addedToCartsProducts)
+        );
+      } else {
+        localStorage.setItem('addedToCart', JSON.stringify([this.product]));
+      }
+    }else {
+      this.router.navigate(["/credential/login"])
+    }
+  }
 }
